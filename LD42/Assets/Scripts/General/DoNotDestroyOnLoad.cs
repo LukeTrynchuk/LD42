ee2b8813b3712df87437a33782bd468fee0215ad
Unknown <luke123t@gmail.com>
2018-08-10 20:27:45 -0300

    Add DoNotDestroyOnLoad script for services

---
+++ LD42/Assets/Scripts/General/DoNotDestroyOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RoboCorp.General
+{
+    /// <summary>
+    /// Do not destroy on load will on awake
+    /// make the current gameobject this script
+    /// is attached to not be destroyed on load.
+    /// </summary>
+    public class DoNotDestroyOnLoad : MonoBehaviour
+    {
+		#region Main Methods
+		private void Awake()
+		{
+            DontDestroyOnLoad(gameObject);
+		}
+		#endregion
+	}
+}
