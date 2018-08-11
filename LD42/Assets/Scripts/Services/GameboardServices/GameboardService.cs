657d9144a3d12065e828cf4da25aa7b8706be30d
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-08-11 15:02:40 -0300

    Gameboard service

---
+++ LD42/Assets/Scripts/Services/GameboardServices/GameboardService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboCorp.Core.Services;
+using RoboCorp.Gameboard;
+
+namespace RoboCorp.Services
+{
+    public class GameboardService : MonoBehaviour, IGameboardService {
+
+        private List<Entity> entityList = new List<Entity>();
+        #region MainMethods
+
+        void Awake() => RegisterService();
+        public void RegisterEntity(Entity entity)
+        {
+            entityList.Add(entity);
+        }
+
+        public void RegisterService()
+        {
+            ServiceLocator.Register<IGameboardService>(this);
+        }
+        #endregion
+
+    }
+}
+
---
+++ LD42/Assets/Scripts/Services/GameboardServices/IGameboardService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboCorp.Gameboard;
+using RoboCorp.Core.Services;
+
+namespace RoboCorp.Services
+{
+    public interface IGameboardService : IService {
+
+        void RegisterEntity(Entity entity);
+    }
+}
+
