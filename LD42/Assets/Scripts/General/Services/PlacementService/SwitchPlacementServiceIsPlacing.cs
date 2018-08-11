ece2a89a0d39fd0746fdaa394f0918bcb2e9d9aa
Unknown <luke123t@gmail.com>
2018-08-11 10:35:49 -0300

    Add switch placing value utility when build button is clicked

---
+++ LD42/Assets/Scripts/General/Services/PlacementService/SwitchPlacementServiceIsPlacing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using RoboCorp.Core.Services;
+using RoboCorp.Services;
+
+namespace RoboCorp.General
+{
+    /// <summary>
+    /// The Switch placement service is placing 
+    /// component will switch the value of IsPlacing
+    /// to the opposite value. 
+    /// </summary>
+    public class SwitchPlacementServiceIsPlacing : MonoBehaviour
+    {
+        #region Private Variables
+        private ServiceReference<IPlacementService> m_placementService
+                        = new ServiceReference<IPlacementService>();
+        #endregion
+
+        #region Main Methods
+        public void SwitchIsPlacingValue()
+        {
+            if (!m_placementService.isRegistered()) return;
+
+            bool currentValue = m_placementService.Reference.IsPlacing;
+            m_placementService.Reference.SetPlacingActive(!currentValue);
+        }
+        #endregion
+    }
+}
