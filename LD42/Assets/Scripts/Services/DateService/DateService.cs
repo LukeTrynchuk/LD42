d9fe4b6457cb72626bc3f8ab06e42b35dd6f51f8
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-08-11 16:57:02 -0300

    Added the Wallet and date Services

---
+++ LD42/Assets/Scripts/Services/DateService/DateService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboCorp.Core.Services;
+
+namespace RoboCorp.Services
+{
+    public class DateService : MonoBehaviour, IDateService
+    {
+        public void RegisterService()
+        {
+            ServiceLocator.Register<IDateService>(this);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ LD42/Assets/Scripts/Services/DateService/IDateService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboCorp.Core.Services;
+
+namespace RoboCorp.Services
+{
+    public interface IDateService : IService
+    {
+        
+    }
+}
+
