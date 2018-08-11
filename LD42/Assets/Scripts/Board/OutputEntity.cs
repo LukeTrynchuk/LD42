a34da1ebd7517ced3a1619fcb7a970bb71edb58c
Unknown <luke123t@gmail.com>
2018-08-10 22:51:55 -0300

    Add output entity

---
+++ LD42/Assets/Scripts/Board/OutputEntity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace RoboCorp.Gameboard
+{
+    /// <summary>
+    /// The output entity is a gameboard entity
+    /// that given an input will sell for
+    /// money.
+    /// </summary>
+    public class OutputEntity : Entity
+    {
+        #region Main Methods
+        public override void Animate() {}
+
+        public override void Tick() {}
+        #endregion
+    }
+}
