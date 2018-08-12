8018b7e27e8466fa03fea2acaa67be8c1aa88cf4
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-08-12 17:58:46 -0300

    Main Menu

---
+++ LD42/Assets/Scripts/SoundManagerService/ScritableAudioObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Audio/Object",fileName = "AudioClip")]
+public class ScritableAudioObject : ScriptableObject {
+
+    public string id;
+    public AudioClip clip;
+
+}
