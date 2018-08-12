972e932ab468ca1e726396a68c4dd83bcc127820
Unknown <luke123t@gmail.com>
2018-08-12 20:39:33 -0300

    Add Processor to build menu

---
+++ LD42/Assets/Scripts/Board/ProcessorEntity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoboCorp.Gameboard
+{
+    /// <summary>
+    /// The Processor Entity is a processor 
+    /// that will process a resource into something
+    /// of higher value.
+    /// </summary>
+    public class ProcessorEntity : Entity
+    {
+        #region Private Variables
+        #endregion
+
+        #region Main Methods
+        public override void Animate() {}
+
+        public override void Tick()
+        {
+            TransportResource();
+            Animate();
+        }
+
+		public override void TransportResource()
+		{
+            if(m_forwardOutput == null)
+            {
+                if (resourceContainer.OldResourceList.Count <= 0) return;
+                DestroyResource();
+                return;
+            }
+
+            resourceContainer.TransferResource(m_forwardOutput.ResourcesContainer);
+            m_forwardOutput.TransportResource();
+		}
+		#endregion
+	}
+}
