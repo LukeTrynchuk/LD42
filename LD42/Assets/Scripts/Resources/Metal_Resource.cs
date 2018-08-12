c4dabaf6448cb6050c7b1f732a1329e56b36fcfa
Unknown <luke123t@gmail.com>
2018-08-11 21:30:06 -0300

    Add Resource Prefabs

---
+++ LD42/Assets/Scripts/Resources/Metal_Resource.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace RoboCorp.Resources
+{
+    /// <summary>
+    /// The Metal resource is a ball of scrap
+    /// metal that can be refined into sheet metal
+    /// and used to create different body parts.
+    /// </summary>
+    public class Metal_Resource : Resource
+    {
+        
+    }
+}
---
+++ LD42/Assets/Scripts/Resources/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoboCorp.Resources
+{
+    /// <summary>
+    /// A resource is a unit of something
+    /// that is used to create other higher
+    /// value items.
+    /// </summary>
+    public abstract class Resource : MonoBehaviour
+    {
+        #region Main Methods
+        #endregion
+    }
+}
