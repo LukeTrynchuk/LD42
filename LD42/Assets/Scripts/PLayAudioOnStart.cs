a47ddf0e03cf706d0e2b5f184e162ac9d8441cc4
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-08-12 18:16:39 -0300

    Everything set up again

---
+++ LD42/Assets/Scripts/PLayAudioOnStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboCorp.Core.Services;
+using RoboCorp.Services;
+
+public class PLayAudioOnStart : MonoBehaviour {
+
+    private ServiceReference<ISoundManagerService> soundManager = new ServiceReference<ISoundManagerService>();
+	// Use this for initialization
+
+	void Start () {
+
+        soundManager.AddRegistrationHandle(RegisterSoundManager);
+
+        soundManager.Reference.Play("Music1");
+	}
+
+    private void RegisterSoundManager()
+    {
+        soundManager.Reference.RegisterService();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
