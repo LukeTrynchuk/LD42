a53e9888b5be1d8ff8f52d52638554728270a642
Unknown <luke123t@gmail.com>
2018-08-12 21:08:44 -0300

    Add refining metal

--- LD42/Assets/Scripts/Resources/Uranium_Resource.cs
+++ LD42/Assets/Scripts/Resources/Uranium_Resource.cs
@@ -6,7 +6,7 @@ namespace RoboCorp.Resources
     /// The Uranimum resource is brought in and
     /// can be refined to highly reactive uranium.
     /// </summary>
-    public class Uranium_Resource : MonoBehaviour 
+    public class Uranium_Resource : Resource 
     {
         
     }
