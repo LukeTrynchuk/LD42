99aade73909f4e9e8d0aed1144dd3574e9aa01ee
Unknown <luke123t@gmail.com>
2018-08-26 11:53:30 -0300

    Clean up Audio Utility Files

---
+++ LD42/Assets/Scripts/Scenes/AdvanceScene.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RoboCorp.Scenes
+{
+
+    /// <summary>
+    /// The Advance Scene component 
+    /// will advance the scene
+    /// to the next scene in the 
+    /// scene build list.
+    /// </summary>
+    public class AdvanceScene : MonoBehaviour
+    {
+        #region Main Methods
+        public void Advance()
+        {
+            int sceneIndex = GetCurrentSceneIndex();
+            sceneIndex++;
+            AttemptSceneLoad(sceneIndex);
+        }
+        #endregion
+
+        #region Utility Methods
+        private int GetCurrentSceneIndex()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            return currentScene.buildIndex;
+        }
+
+        private void AttemptSceneLoad(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+        #endregion
+    }
+}
